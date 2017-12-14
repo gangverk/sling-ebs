35f29fd0b6a717f73d6f9b80dbc74e2ab3f44ee3
tomasdarri27 <tomast14@ru.is>
2017-12-14 20:11:55 +0000

    small changes

--- src/components/MessageModal/index.js
+++ src/components/MessageModal/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
+import greenIcon from './task-green.svg';
 import * as ApiActions from '../../components/Actions/actions';
 import closeX from '../Modal/x.png';
 
@@ -35,7 +35,32 @@ const MessageWrapper = styled.div`
   margin: auto;
   border: 2px solid #dadada;
   border-radius: 5px;
-  width: 550px;
+  width: 461px;
+  height: 360px;
+  min-width: 461px;
+  min-height: 360px;
+  display: flex;
+  justify-content: space-between;
+  button {
+    border: none;
+    background-color: Transparent;
+    cursor: pointer;
+    padding: 0;
+    outline: none;
+    margin-right: 4%;
+  }
+  p {
+    font-family: Trebuchet MS;
+    font-size: 20px;
+    margin-left: 1em;
+    color: #738190;
+    justify-content: center;
+    align-items: center;
+    margin-top: 193px;
+  }
+  .greenIcon {
+    margin-top: -119px;
+  }
 `;
 
 class MessageModal extends Component {
@@ -69,10 +94,11 @@ class MessageModal extends Component {
     return (
       <ModalWrapper visable={this.state.visable}>
         <MessageWrapper>
+          <img className="greenIcon" alt="greenIcon" src={greenIcon} />
+          <p>{this.props.modalText}</p>
           <button onClick={() => this.setState({ visable: false })}>
             <img alt="X button" src={closeX} />
           </button>
-          <p>{this.props.modalText}</p>
         </MessageWrapper>
       </ModalWrapper>
     );
