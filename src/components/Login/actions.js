94611903ccb73ef238af9b0d5531818f6eed45c3
Hreinn Rúnarsson <hreinnr13@ru.is>
2017-12-15 01:16:51 +0000

    refresh working store in local storage

--- src/components/Login/actions.js
+++ src/components/Login/actions.js
@@ -1,6 +1,7 @@
 import * as actionTypes from './actionTypes';
 
 export const setUserData = (name, email, picture, id) => {
+  console.log('kallað í setuserdata', name, email, picture, id);
   return dispatch => {
     dispatch({
       type: actionTypes.FACEBOOK_DATA,
