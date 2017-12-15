31edc451c49751a16bc7229036df6cb671246abc
Hreinn Rúnarsson <hreinnr13@ru.is>
2017-12-15 05:06:15 +0000

    clean up

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.button`
-    border: 1px solid black;
-    &:hover {
-        border: 1px solid red;
-    }
-`;
--- src/containers/about/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class About extends Component {
-  render() {
-    return <p>This is about us page</p>;
-  }
-}
