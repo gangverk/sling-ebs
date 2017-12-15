28c08703ddcf61ff169fb031c0ecf92f8dec7984
Hreinn Rúnarsson <hreinnr13@ru.is>
2017-12-15 05:15:55 +0000

    please

---
+++ src/containers/home/actionTypes.js
@@ -0,0 +1,5 @@
+export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED';
+export const INCREMENT = 'counter/INCREMENT';
+export const INCREMENTBY = 'counter/INCREMENTBY';
+export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED';
+export const DECREMENT = 'counter/DECREMENT';
